8f92dab4f4d5f75ae25a5d3348e24eaf5fe840ad
Paul <pavkukharenko@gmail.com>
2021-01-17 23:52:24 +0700

    solve sqr kata

--- python/youAreASquare/main.py
+++ python/youAreASquare/main.py
@@ -1,2 +1,12 @@
-def is_square(n):    
-    return False # fix me
+import math
+
+def is_square(n): 
+    if(n < 0):
+        return False   
+    elif(math.sqrt(n) % 1 == 0):
+        return True
+    else:
+        return False
+
+
+print(is_square(-1))
