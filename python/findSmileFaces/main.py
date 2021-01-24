71f2101e17ab2ababf971261f97f46ad3c5b58ff
Paul <pavkukharenko@gmail.com>
2021-01-24 10:30:36 +0700

    solve smile faces kata

--- python/findSmileFaces/main.py
+++ python/findSmileFaces/main.py
@@ -1,2 +1,9 @@
 def count_smileys(arr):
-    return #the number of valid smiley faces in array/list
+    count = 0
+    smile_list = [':)', ';)', ':-)', ';-)', ':~)', ';~)', ':D', ';D', ':-D', ';-D', ':~D', ';~D']
+    for i in arr:
+        if i in smile_list:
+            count = count + 1
+    return count
+
+print(count_smileys([':)', ';(', ';}', ':-D']))
