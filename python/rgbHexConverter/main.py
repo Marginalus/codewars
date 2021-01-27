472975fca7ce45eef5b8910925abf7b40f40655c
Paul <pavkukharenko@gmail.com>
2021-01-27 16:06:51 +0700

    solve rgb hex converter kata

--- python/rgbHexConverter/main.py
+++ python/rgbHexConverter/main.py
@@ -1,3 +1,16 @@
 def rgb(r, g, b):
-    #your code here :)
-    pass
+    r = clamp(r)
+    g = clamp(g)
+    b = clamp(b)
+    string = '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return string
+
+def clamp(x):
+    if x < 0:
+        return 0
+    elif x > 255:
+        return 255
+    else:
+        return x
+
+print(rgb(-20,275,125))    
